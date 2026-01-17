8d531f67d78ea1ebee22cb092d7b755a23278661
AmmarA06 <ahmadammar2279@gmail.com>
2026-01-17 11:12:07 -0500

    add api config for mock

---
+++ mock-website/config.example.js
@@ -0,0 +1,4 @@
+window.AMPLITUDE_CONFIG = {
+    apiKey: 'YOUR_ACTUAL_API_KEY_HERE',
+    serverZone: 'US'
+};
---
+++ mock-website/config.js
@@ -0,0 +1,4 @@
+window.AMPLITUDE_CONFIG = {
+    apiKey: '6535665d9a4aea8453dcb43c823095a',
+    serverZone: 'US'
+};
